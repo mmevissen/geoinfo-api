c2f07dc8e87354d455270a820b5f4cdf9ede404c
matthesrieke <m.rieke@52north.org>
2017-06-07 13:16:48 +0200

    add concurrent test example

--- src/test/java/de/hsbochum/fbg/kswe/geoinfo/api/WeatherRetrieverImplTest.java
+++ src/test/java/de/hsbochum/fbg/kswe/geoinfo/api/WeatherRetrieverImplTest.java
@@ -2,6 +2,13 @@
 package de.hsbochum.fbg.kswe.geoinfo.api;
 
 import java.io.IOException;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicReference;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -11,13 +18,39 @@ import org.junit.Test;
 public class WeatherRetrieverImplTest {
     
     @Test
-    public void testRetrieval() throws UnsupportedCountryException, IOException {
+    public void testRetrieval() throws UnsupportedCountryException, IOException, InterruptedException, BrokenBarrierException, TimeoutException {
         WeatherRetrieverImpl retriever = new WeatherRetrieverImpl();
+        final CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
         
-        Weather weather = retriever.retrieve(new City("Bochum", "Germany"));
-        float temp = weather.getTemp();
+        final AtomicReference<Float> temp = new AtomicReference<>();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Weather weather;
+                try {
+                    weather = retriever.retrieve(new City("Bochum", "Germany"));
+                    temp.set(weather.getTemp());
+                    
+                    Thread.sleep(3000);
+                    
+                    cyclicBarrier.await();
+                } catch (UnsupportedCountryException ex) {
+                    ex.printStackTrace();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                } catch (BrokenBarrierException ex) {
+                    ex.printStackTrace();
+                }
+                
+            }
+        }).start();
+        
+        cyclicBarrier.await(5, TimeUnit.SECONDS);
         
         //check and assert a range that is suitable for °C
+        Assert.assertThat(temp.get() >= 60, CoreMatchers.is(true));
     }
 
 }
